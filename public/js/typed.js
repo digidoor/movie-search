e2811f87569c7a7dad03fbf016d3f4e9e094ddea
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-03-31 16:38:53 -0700

    main page is added

---
+++ public/js/typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".mutiple-text", {
+    strings: ["Alan Busovaca", "Farid Gardoon", "Gary", "Masih Omer", "Mohammad Ghaznawy", "Nicholas"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
