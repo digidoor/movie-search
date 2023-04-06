65f2d900c2ac5f8133e1a2a981c6915b94fc2445
unknown <nmukhtar2001@gmail.com>
2023-04-05 22:12:54 -0700

    fix a bug

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -25,7 +25,6 @@ router.post('/login', async (req, res) => {
   try {
     // Find the user who matches the posted e-mail address
     const userData = await User.findOne({ where: { email: req.body.email } });
-
     if (!userData) {
       res
         .status(400)
@@ -38,7 +37,7 @@ router.post('/login', async (req, res) => {
 
     if (!validPassword) {
       res
-        .status(400)
+        .status(401)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
@@ -47,12 +46,12 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
-    res.status(400).json(err);
+    res.status(402).json(err);
   }
 });
 
--- seeds/seed.js
+++ seeds/seed.js
@@ -3,7 +3,7 @@ const { User } = require('../models');
 const seedMovie = require('./movieData');
 
 const userData = require('./userData.json');
-
+console.log(userData)
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
