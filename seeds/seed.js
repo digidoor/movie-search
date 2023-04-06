c0c20739f895904f22198c78d179a6144c6bff21
digidoor <37601466+digidoor@users.noreply.github.com>
2023-04-05 22:32:46 -0700

    fix weird merge conflict hopefully

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,11 +3,7 @@ const { User } = require('../models');
 const seedMovie = require('./movieData');
 
 const userData = require('./userData.json');
-<<<<<<< HEAD
 console.log(userData);
-=======
-console.log(userData)
->>>>>>> 332cd222c98166c8b1748d626fd28b32b37503f1
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
