28f09070bb197b0acaf21c9895ccd555273c07b0
digidoor <37601466+digidoor@users.noreply.github.com>
2023-04-11 14:17:56 -0700

    revert error codes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
       res.status(200).json(userData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
@@ -37,11 +37,12 @@ router.post('/login', async (req, res) => {
 
     if (!validPassword) {
       res
-        .status(400)
+        .status(401)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
+    // Create session variable based on loggin in user
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
@@ -50,12 +51,12 @@ router.post('/login', async (req, res) => {
     });
 
   } catch (err) {
-    res.status(400).json(err);
+    res.status(402).json(err);
   }
 });
 
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
+  if (req.session.logged_in) { //Remove the session variables
     req.session.destroy(() => {
       res.status(204).end();
     });
