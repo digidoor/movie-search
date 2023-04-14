d9cfc61d48f42d0ad610d851afa4ea4f8a13eb58
digidoor <37601466+digidoor@users.noreply.github.com>
2023-04-14 16:30:11 -0700

    reverted back to https; doesn't work with http.

--- public/js/movies.js
+++ public/js/movies.js
@@ -6,7 +6,7 @@ const saveBtn = document.getElementById('saveBtn')
 
 // load movies from API
 async function loadMovies(searchTerm){
-    const URL = `http://omdbapi.com/?s=${searchTerm}&page=1&apikey=6fb32665`;
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=6fb32665`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
     // console.log(data.Search);
